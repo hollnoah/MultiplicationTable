64424d64ee659aa34b82b1f2376563d371885fab
Noah Holloway <noahholloway23@gmail.com>
2025-02-05 15:21:25 -0700

    add header and attempted code

---
+++ MultiplicationTable/MultiplicationTable.vb
@@ -0,0 +1,19 @@
+﻿'Noah Holloway
+'RCET 2265
+'Spring 2025
+'MultiplicationTable
+
+Option Explicit On
+Option Strict On
+Option Compare Text
+Module MultiplicationTable
+
+    Sub Main()
+        Dim userInput As Integer
+        For i = userInput To userInput
+
+        Next
+        Console.ReadLine()
+    End Sub
+
+End Module

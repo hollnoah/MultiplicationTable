2e4a978f8d624ea2d14aa6ffe670ae3f2894fb0f
Noah Holloway <noahholloway23@gmail.com>
2025-03-14 10:52:17 -0600

    repsuh since assignment didn't submit

--- MultiplicationTable/MultiplicationTable.vb
+++ MultiplicationTable/MultiplicationTable.vb
@@ -9,11 +9,42 @@ Option Compare Text
 Module MultiplicationTable
 
     Sub Main()
-        Dim userInput As Integer
-        For i = userInput To userInput
+        Dim userInput As String
+        Dim tableSize As Integer
+        Dim validInput As Boolean = False
 
+
+        Do
+
+            Console.WriteLine("Please enter a positive whole number")
+            userInput = Console.ReadLine()
+            Try
+                tableSize = CInt(userInput)
+                If tableSize > 0 Then
+                    validInput = True
+                Else
+                    Console.WriteLine("Looks like someone can't follow directions... ")
+
+                End If
+
+            Catch ex As Exception
+                Console.WriteLine()
+            End Try
+
+        Loop Until validInput
+
+        Console.WriteLine()
+
+        Console.WriteLine("Enjoy your " & userInput & " " & "*" & " " & userInput & " " & "Multiplication table:")
+        For i As Integer = 1 To tableSize
+            For j As Integer = 1 To tableSize
+                Console.Write((i * j).ToString().PadLeft(5))
+
+            Next
+            Console.WriteLine()
         Next
-        Console.ReadLine()
+
+
     End Sub
 
 End Module
